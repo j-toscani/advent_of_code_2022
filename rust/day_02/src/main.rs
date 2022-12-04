ad6f72d4ae72d1c094c0cb421214314677df6fc8
Julian Toscani <julian.toscani@rtl.com>
2022-12-04 19:58:42 +0100

    add solution for excercise 2

--- rust/day_02/src/main.rs
+++ rust/day_02/src/main.rs
@@ -15,31 +15,9 @@ enum GameResult {
 }
 
 struct Matchup {
-    opponent: Symbol,
+    _opponent: Symbol,
     me: Symbol,
     result: GameResult
-}
-
-impl Matchup {
-    fn get_result(&self) -> GameResult{
-        match self.opponent {
-            Symbol::Rock => match self.me {
-                Symbol::Paper => GameResult::Win,
-                Symbol::Scissors => GameResult::Lose,
-                _ => GameResult::Draw,
-            },
-            Symbol::Paper => match self.me {
-                Symbol::Scissors => GameResult::Win,
-                Symbol::Rock => GameResult::Lose,
-                _ => GameResult::Draw,
-            },
-            Symbol::Scissors => match self.me {
-                Symbol::Rock => GameResult::Win,
-                Symbol::Paper => GameResult::Lose,
-                _ => GameResult::Draw,
-            },
-        }
-    }
 }
 
 struct Game {
@@ -61,28 +39,63 @@ impl Game {
         score
     }
 
-    fn parse_match_up(&mut self, string: &str) {
-        let chars = string.chars().collect::<Vec<char>>();
-        let first = chars.get(0).unwrap().to_string();
-        let last = chars.get(2).unwrap().to_string();
+    fn parse_match_up_strat_two(&mut self, string: &str) {
+        let (first, last) = get_firs_and_last(string);
 
-        let opponent = get_oponent_symbol(first.as_str()).unwrap();
+        let opponent = get_opponent_symbol(first.as_str()).unwrap();
+        let result = get_my_result(last.as_str()).unwrap();
         
-        let me = (match last.as_str() {
-            "X" => Ok(Symbol::Rock),
-            "Y" => Ok(Symbol::Paper),
-            "Z" => Ok(Symbol::Scissors),
-            _ => Err("Unknown Symbol")
-        }).unwrap();
-
-        let result = get_game_result(&opponent, &me);
+        let me = get_my_symbol_from_game_result(&opponent, &result);
 
         self.matchups.push(Matchup {
-            opponent,
+            _opponent: opponent,
             me,
             result
         });
     }
+
+    fn parse_match_up_strat_one(&mut self, string: &str) {
+        let (first, last) = get_firs_and_last(string);
+
+        let opponent = get_opponent_symbol(first.as_str()).unwrap();
+        let me = get_my_symbol(&last.as_str()).unwrap();
+
+        let result = get_game_result(&opponent, &me);
+
+        self.matchups.push(Matchup {
+            _opponent: opponent,
+            me,
+            result
+        });
+    }
+}
+
+fn get_my_symbol_from_game_result(opponent: &Symbol, result: &GameResult) -> Symbol {
+    match opponent {
+        Symbol::Rock => match result {
+            GameResult::Win => Symbol::Paper,
+            GameResult::Lose => Symbol::Scissors,
+            _ => Symbol::Rock,
+        },
+        Symbol::Paper => match result {
+            GameResult::Win => Symbol::Scissors,
+            GameResult::Lose => Symbol::Rock,
+            _ => Symbol::Paper,
+        },
+        Symbol::Scissors => match result {
+            GameResult::Win => Symbol::Rock,
+            GameResult::Lose => Symbol::Paper,
+            _ => Symbol::Scissors,
+        },
+    }
+}
+
+fn get_firs_and_last(string: &str) -> (String, String) {
+    let chars = string.chars().collect::<Vec<char>>();
+    let first = chars.get(0).unwrap().to_string();
+    let last = chars.get(2).unwrap().to_string();
+
+    return (first, last)
 }
 
 fn get_game_result(opponent: &Symbol, me: &Symbol) -> GameResult{
@@ -105,7 +118,25 @@ fn get_game_result(opponent: &Symbol, me: &Symbol) -> GameResult{
     }
 }
 
-fn get_oponent_symbol(string: &str) -> Result<Symbol, &str> {
+fn get_my_symbol(string: &str) -> Result<Symbol, &str> {
+    match string {
+        "X" => Ok(Symbol::Rock),
+        "Y" => Ok(Symbol::Paper),
+        "Z" => Ok(Symbol::Scissors),
+        _ => Err("Unknown Symbol")
+    }
+}
+
+fn get_my_result(string: &str) -> Result<GameResult, &str> {
+    match string {
+        "X" => Ok(GameResult::Lose),
+        "Y" => Ok(GameResult::Draw),
+        "Z" => Ok(GameResult::Win),
+        _ => Err("Unknown Symbol")
+    }
+}
+
+fn get_opponent_symbol(string: &str) -> Result<Symbol, &str> {
     match string {
         "A" => Ok(Symbol::Rock),
         "B" => Ok(Symbol::Paper),
@@ -137,14 +168,18 @@ fn main() {
     let data = File::open(cwd).unwrap();
     let reader = BufReader::new(data);
 
-    let mut game = Game::new();
+    let mut game_one = Game::new();
+    let mut game_two = Game::new();
 
     for line in reader.lines() {
         let line_string = line.unwrap();
-        game.parse_match_up(&line_string);
+        game_one.parse_match_up_strat_one(&line_string);
+        game_two.parse_match_up_strat_two(&line_string)
     }
 
-    let score = game.calculate_scores();
+    let score_one = game_one.calculate_scores();
+    let score_two = game_two.calculate_scores();
 
-    println!("{}", score);
+    println!("Score of game one is: {}", score_one);
+    println!("Score of game two is: {}", score_two);
 }
