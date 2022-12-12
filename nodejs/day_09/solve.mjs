b9077e03e64dbab4dc6a789317d632ba601f8e81
Julian Toscani <julian.toscani@rtl.com>
2022-12-12 17:04:23 +0100

    try part 2 and fail

--- nodejs/day_09/solve.mjs
+++ nodejs/day_09/solve.mjs
@@ -1,21 +1,32 @@
 import fs from "fs";
 
-const data = fs.readFileSync("./data.txt", { encoding: "utf-8" });
+const [matrixString, rawInstructionString] = fs.readFileSync("./data.txt", { encoding: "utf-8" }).split("\n\n");
+const matrix = matrixString.split("\n").map(string => string.split(""));
 
-const rawInstructions = data.split("\n");
+const rawInstructions = rawInstructionString.split("\n");
 const instructions = rawInstructions.map(parseInstruction);
-const ropeOne = new Array(2).fill({ x: 0, y: 0 });
-const ropeTwo = new Array(9).fill({ x: 0, y: 0 });
+
+const ropeOne = [
+  { x: 0, y: 0 },
+  { x: 0, y: 0 },
+];
+const ropeTwo = [
+  { x: 0, y: 4 },
+  { x: 0, y: 4 },
+  { x: 0, y: 4 },
+  { x: 0, y: 4 },
+  { x: 0, y: 4 },
+  { x: 0, y: 4 },
+  { x: 0, y: 4 },
+  { x: 0, y: 4 },
+  { x: 0, y: 4 },
+  { x: 0, y: 4 },
+];
 const positions = new Set();
 
-instructions.forEach(applyInstructionTaskOne());
+instructions.forEach(applyInstructionTaskOne(ropeTwo));
 
-console.log(positions.size);
-
-function applyInstructionTaskOne() {
-  const head = { x: 0, y: 0 };
-  const tail = { x: 0, y: 0 };
-
+function applyInstructionTaskOne(rope) {
   return (instruction) => {
     const { x, y } = instruction;
 
@@ -23,27 +34,37 @@ function applyInstructionTaskOne() {
       console.error("Wrong input. Doing nothing");
       return;
     }
-
     const direction = x ? "x" : "y";
     const stepValue = instruction[direction] > 0 ? 1 : -1;
     const steps = new Array(Math.abs(instruction[direction])).fill(stepValue);
 
+    let head = rope[0];
+    
     for (let step of steps) {
       head[direction] += step;
+      let tmpHead = head;
 
-      moveTail(tail, head);
-      positions.add(`${tail.x} ${tail.y}`);
+      for (let index = 1; index < rope.length; index++) {
+        const tail = rope[index];
+        moveTail(tail, tmpHead);
+
+        tmpHead = tail;
+      }
+
+      positions.add(`${rope.at(-1).x}, ${rope.at(-1).y}`)
     }
+
+    console.log(rope);
   };
 }
 
-function moveTail(tail, head) {
+function moveTail(tail, head, step) {
   const distance = {
     x: tail.x - head.x,
     y: tail.y - head.y,
   };
 
-  if (Math.abs(distance.x) <= 1 && Math.abs(distance.y) <= 1) {
+  if (Math.abs(distance.x) < 1 && Math.abs(distance.y) < 1) {
     return;
   }
 
@@ -51,14 +72,14 @@ function moveTail(tail, head) {
     if (Math.abs(distance.y) === 1) {
       tail.y = head.y;
     }
-    tail.x += distance.x;
+    tail.x += step;
   }
 
   if (Math.abs(distance.y) > 1) {
     if (Math.abs(distance.x) === 1) {
       tail.x = head.x;
     }
-    tail.y += distance.y;
+    tail.y += step;
   }
 }
 
@@ -69,13 +90,13 @@ function parseInstruction(instruction) {
 
   switch (rawDirection) {
     case "U":
-      vector = { y: steps, x: 0 };
+      vector = { y: steps * -1, x: 0 };
       break;
     case "R":
       vector = { x: steps, y: 0 };
       break;
     case "D":
-      vector = { y: steps * -1, x: 0 };
+      vector = { y: steps, x: 0 };
       break;
     case "L":
       vector = { x: steps * -1, y: 0 };
